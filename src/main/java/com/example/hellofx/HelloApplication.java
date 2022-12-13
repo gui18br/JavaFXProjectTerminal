cd824f90cba0a26afe0889d617a57bd5a5e1f755
guiga18br <guilhermenovaislimaa@hotmail.com>
2022-12-12 22:24:14 -0300

    Second commit

--- src/main/java/com/example/hellofx/HelloApplication.java
+++ src/main/java/com/example/hellofx/HelloApplication.java
@@ -10,10 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("primeiroMenu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("primeiroMenu.fxml"));
+        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("pTerminal.fxml"));
+
+        Scene scene1 = new Scene(fxmlLoader1.load());
+        Scene scene2 = new Scene(fxmlLoader2.load());
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(scene1);
         stage.show();
     }
 
